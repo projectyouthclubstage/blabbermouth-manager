76efe4c8f52e543e890f2dec0c5a52344b5d4aad
Sascha Deeg <sascha.deeg@gmail.com>
2019-08-13 23:27:16 +0200

    Index angelegt

--- src/main/java/de/youthclubstage/backend/blabbermouthmanagerservice/entity/Message.java
+++ src/main/java/de/youthclubstage/backend/blabbermouthmanagerservice/entity/Message.java
@@ -3,6 +3,7 @@ package de.youthclubstage.backend.blabbermouthmanagerservice.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -12,14 +13,18 @@ import java.util.UUID;
 public class Message {
     @Id
     private UUID id;
+    @Indexed
     private Integer process;
+    @Indexed
     private Integer state;
+    @Indexed
     private String application;
     private String content;
     private Long version;
     private Long retryCount = 0L;
     private boolean isRetryMessage = false;
     private UUID previousMessage;
+    @Indexed
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="CET")
     private Calendar calendar;
 }
